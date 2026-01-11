02e34433fa868dc399faf154780c8e5ec3b2a8cb
Dasha <daryna2003tk@gmail.com>
2026-01-11 12:53:30 +0100

    finished

--- cypress/e2e/shopping.cy.js
+++ cypress/e2e/shopping.cy.js
@@ -72,7 +72,7 @@ describe('E-Commerce Shopping Flow', () => {
       cy.contains('Add to Cart').click();
     });
     cy.get('[data-testid="cart-icon"]').click();
-    cy.get('button').contains('+').click();
+    cy.contains('button', '+').click();
     cy.wait(500);
     cy.get('[data-testid="cart-count"]').should('contain', '2');
   });
@@ -83,7 +83,7 @@ describe('E-Commerce Shopping Flow', () => {
       cy.contains('Add to Cart').click();
     });
     cy.get('[data-testid="cart-icon"]').click();
-    cy.contains('Remove').click();
+    cy.contains('button', 'Remove').click();
     cy.contains('Removed from cart').should('be.visible');
   });
 
